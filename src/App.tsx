a88efe0c20952d794574f16d8b8e24ea6caa7c20
KausaChangs <changayakausa@gmail.com>
2022-12-26 13:12:10 +0200

    added a navigate to route that redirects to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<h1>Hi</h1>} />
       <Route path="/new" element={<h1>New</h1>} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
