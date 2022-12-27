57e7fb80faa94a3fb12d61dd54228713e72be59d
KausaChangs <changayakausa@gmail.com>
2022-12-27 08:22:08 +0200

    added routing for ids

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import { Routes, Route, Navigate } from "react-router-dom";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Hi</h1>} />
+      <Route path="/" element={<h1>Home</h1>} />
       <Route path="/new" element={<h1>New</h1>} />
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
