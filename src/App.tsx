12f1a83ea60f9d08baeab691da2a66962c3519bf
KausaChangs <changayakausa@gmail.com>
2022-12-29 10:43:07 +0200

    added a new note component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { NewNote } from "./newNote";
 
 function App() {
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="/new" element={<NewNote />} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
---
+++ src/newNote.tsx
@@ -0,0 +1,3 @@
+export function NewNote() {
+  return <h1>Hiii</h1>;
+}
