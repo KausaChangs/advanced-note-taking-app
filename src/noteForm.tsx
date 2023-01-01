2a6603e3812baa351e0423fdb73d4bf72d67a030
KausaChangs <changayakausa@gmail.com>
2023-01-01 13:28:05 +0200

    added standed boiler plate  to note form

--- src/noteForm.tsx
+++ src/noteForm.tsx
@@ -1,9 +1,15 @@
-import { Form, Stack } from "react-bootstrap";
+import { Col, Form, FormGroup, Row, Stack } from "react-bootstrap";
 
 export function NoteForm() {
   return (
     <Form>
-      <Stack gap={4}></Stack>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group></Form.Group>
+          </Col>
+        </Row>
+      </Stack>
     </Form>
   );
 }
