2d61d5a87438a81b7f4f3a32904c23e1d9e204fd
KausaChangs <changayakausa@gmail.com>
2023-03-07 15:41:15 +0200

    added logic to noteList

--- src/noteList.tsx
+++ src/noteList.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import ReactSelect from "react-select";
+import { Tag } from "./App";
 
-export function NoteList() {
+type NoteListProps = {
+  availableTags: Tag[];
+};
+
+export function NoteList({ availableTags }: NoteListProps) {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+
   return (
     <>
       <Row>
@@ -23,6 +32,33 @@ export function NoteList() {
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
               <Form.Control type="text" />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <Form.Control />
+              <ReactSelect
+                onCreateOption={(label) => {
+                  const newTag = { id: uuidV4(), label };
+                  onAddTag(newTag);
+                  setSelectedTags((Prev) => [...Prev, newTag]);
+                }}
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+                isMulti
+              />
             </Form.Group>
           </Col>
         </Row>
